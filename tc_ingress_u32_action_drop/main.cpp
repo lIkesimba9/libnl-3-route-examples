73d4c906f3c602520a634509575c69892501a026
simba9 <mail>
2024-07-23 09:40:48 +0300

    remove unused code

--- tc_ingress_u32_action_drop/main.cpp
+++ tc_ingress_u32_action_drop/main.cpp
@@ -21,47 +21,6 @@
 using namespace std;
 
 
-int get_tc_classid(__u32 *h, const char *str)
-{
-    __u32 maj, min;
-    char *p;
-
-    maj = TC_H_ROOT;
-    if (strcmp(str, "root") == 0)
-        goto ok;
-    maj = TC_H_UNSPEC;
-    if (strcmp(str, "none") == 0)
-        goto ok;
-    maj = strtoul(str, &p, 16);
-    if (p == str) {
-        maj = 0;
-        if (*p != ':')
-            return -1;
-    }
-    if (*p == ':') {
-        if (maj >= (1<<16))
-            return -1;
-        maj <<= 16;
-        str = p+1;
-        min = strtoul(str, &p, 16);
-        if (*p != 0)
-            return -1;
-        if (min >= (1<<16))
-            return -1;
-        maj |= min;
-    } else if (*p != 0)
-        return -1;
-
-    ok:
-    *h = maj;
-    return 0;
-}
-
-uint32_t get_tc_classid_wrap(const std::string& handle) {
-    uint32_t id;
-    get_tc_classid(&id, handle.c_str());
-    return id;
-}
 
 void throw_err(int err) {
     if (err) {
